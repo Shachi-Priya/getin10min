a0551703aeaa62aedd138b340e5fbaf6a0f35700
Shachi Priya <shamil4621@gmail.com>
2025-10-15 18:07:16 +0530

    error fixing

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: { unoptimized: true },
+  output: 'export', //  enables static export (replaces next export)
 };
 
 export default nextConfig;
